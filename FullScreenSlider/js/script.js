5ad61ee73f94aef383fe7a5b65210bb2888d1760
anran758 <18219086758@163.com>
2017-08-23 20:15:22 +0800

    updata

--- FullScreenSlider/js/script.js
+++ FullScreenSlider/js/script.js
@@ -1,7 +1,8 @@
 (function($) {
   var PageSwitch = (function() {
     function PageSwitch(element, options) {
-      this.settins = $.extend(true, $.fn.PageSwitch.default, options || {});
+      // 从这里传入 default 默认值.
+      this.settings = $.extend(true, $.fn.PageSwitch.default, options || {});
       this.element = element;
       this.init();
     }
@@ -11,20 +12,74 @@
       // 初始化dom结构, 布局, 分页及绑定事件
       init : function() {
         var me = this;
+
+        // 获取字符串
         console.log(me);
         me.selectors = me.settings.selectors;
-        me.sections
+        me.sections = me.selectors.sections;
+        me.section = me.selectors.section;
+
+        me.direction = me.sections.direction ==
+                       'vertical' ? true : false;
+        me.pagesCount = me.pagesCount();
+        me.index = (me.settings.index >= 0 &&
+                    me.settings.index < pagesCount) ?
+                    me.settings.index : 0;
+
+        // 如果是横屏, 就调用针对横屏的布局
+         if(!me.direction) {
+          me._initLayout();
+         }
+
+         if (me.settings.pagination) {
+          me._initPaging();
+         }
+
+         me._initEvent();
       },
       /* 获取页面数量 */
       pagesCount : function() {
-
+        return this.section.length;
       },
       /* 获取页面的宽或高度(横 / 竖屏滑动) */
-      switchLength : function() {},
+      switchLength : function() {
+        return this.direction ? this.element.height() :
+                                this.element.width();
+      },
 
-      /* 针对横竖屏情况进行布局 */
-      _initLayout : function() {},
-      _initPaging : function() {},
+      /* 针对横屏情况进行布局 */
+      _initLayout : function() {
+        var me = this;
+        var width = (me.pagesCount * 100) + "%",
+            cellWidth = (100 / me.pagesCount).toFixed(2) + "%";
+
+        // 给容器及每个页面设置宽度
+        me.sections.width(width);
+        me.section.width(cellWidth).css('float', 'left');
+      },
+      /* 实现分页结构及css样式 */
+      _initPaging : function() {
+        var me = this,
+            pagesClass  = me.selectors.page.substring(1),
+            activeClass = me.selectors.active.substring(1);
+
+        var pageHTML = '<ul class=' + pagesClass + '>';
+        for (var i = 0; i < me.pagesCount; i++) {
+          pageHTML += "<li></li>";
+        }
+        me.element.append(pageHTML);
+
+        var pages = me.element.find(me.selectors.page);
+        me.pageItem = pages.find('li');
+        me.pageItem.eq(me.index).addClass(me.activeClass);
+
+        // 判断横竖屏
+        if (me.direction) {
+           page.addClass('vertical');
+        } else {
+          page.addClass('horizontal');
+        }
+      },
       _initEvent : function() {}
     };
 
@@ -50,7 +105,7 @@
     });
   };
 
-  $.fn.PageSwitch.default = {
+  $.fn.PageSwitch.defaults = {
     selectors : {
       sections : ".sections",
       section : ".section",
@@ -69,5 +124,5 @@
 
   $(function() {
     $('[data-PageSwitch').PageSwitch();
-  })
+  });
 })(jQuery);
