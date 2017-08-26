c41cc719703119f6891d83d13c390255adae9d50
anran758 <18219086758@163.com>
2017-08-26 22:31:57 +0800

    amendment

--- waterfall flow/js/js/script.js
+++ waterfall flow/js/js/script.js
@@ -10,57 +10,57 @@
 		]
 	};
 
-	Ajax('GET', 'js/script.js' ,dataInt , function(data) {
-		console.log(data)
-	})
+	// Ajax('GET', 'js/script.js' ,dataInt , function(data) {
+	// 	console.log(data)
+	// })
 
-	function Ajax(type, url, data, success, failed) {
-		var xhr = null;
-		if(window.XMLHttpRequest) {
-			xhr = new XMLHttpRequest();
-		} else {
-			// IE6
-			xhr = new ActiveXObject('Microsoft.XMLHTTP');
-		}
-		var type =  type.toUpperCase();
-		var random = Math.random();
+	// function Ajax(type, url, data, success, failed) {
+	// 	var xhr = null;
+	// 	if(window.XMLHttpRequest) {
+	// 		xhr = new XMLHttpRequest();
+	// 	} else {
+	// 		// IE6
+	// 		xhr = new ActiveXObject('Microsoft.XMLHTTP');
+	// 	}
+	// 	var type =  type.toUpperCase();
+	// 	var random = Math.random();
 
-		// 进行转码
-		if (typeof data == 'object') {
-			var str = '';
-			for (var key in data) {
-				str += key + "=" + '&';
-			}
-			data = str.replace(/&$/,'');
-		}
+	// 	// 进行转码
+	// 	if (typeof data == 'object') {
+	// 		var str = '';
+	// 		for (var key in data) {
+	// 			str += key + "=" + '&';
+	// 		}
+	// 		data = str.replace(/&$/,'');
+	// 	}
 
-		if(type == 'GET') {
-			if(data) {
-				xhr.open('GET', url + "?" + data, true);
-			} else {
-				xhr.open('GET', url + "?t=" + data, true);
-			}
-			xhr.send();
-		} else if (type == 'POST') {
-			xhr.open('POST', url, true);
-			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			xhr.send(data);
-		}
+	// 	if(type == 'GET') {
+	// 		if(data) {
+	// 			xhr.open('GET', url + "?" + data, true);
+	// 		} else {
+	// 			xhr.open('GET', url + "?t=" + data, true);
+	// 		}
+	// 		xhr.send();
+	// 	} else if (type == 'POST') {
+	// 		xhr.open('POST', url, true);
+	// 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	// 		xhr.send(data);
+	// 	}
 
-		// 处理返回的数据
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4) {
-				if (xhr.staus == 200) {
-					success(xhr.responseText);
-				} else {
-					if (failed) {
-						failed(xhr.status);
-					}
-				}
-			}
-		}
-	}
-
+	// 	// 处理返回的数据
+	// 	xhr.onreadystatechange = function() {
+	// 		if (xhr.readyState == 4) {
+	// 			if (xhr.staus == 200) {
+	// 				success(xhr.responseText);
+	// 			} else {
+	// 				if (failed) {
+	// 					failed(xhr.status);
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+waterfall("waterfall", ".box");
 	window.onscroll = function() {
 		if(checkScrollSlide()) {
 			var parent = document.getElementById("waterfall");
